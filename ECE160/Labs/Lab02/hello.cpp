65b203ae07c92fc5de42f09fa333a5bd72f7cf10
Reiuiji <reiuiji@gmail.com>
2011-09-09 20:54:17 -0400

    Added final version of hello.cp

---
+++ ECE160/Labs/Lab02/hello.cpp
@@ -0,0 +1,13 @@
+/*
+ I, Daniel Noyes, certify that this is my own work and I have not collaborated with anyone else.
+ */
+
+ #define _CRT_SECURE_NO_WARNINGS
+ 
+ #include <stdio.h>
+ 
+int main()
+ {
+        printf("Hello World!\n");
+ }
+ 
