7356abf9c85d868600142b852541e5264905c672
Reiuiji <reiuiji@gmail.com>
2011-10-01 01:08:56 -0400

    Updated Convert with fancy boxes

--- ECE160/Projects/Project02/convert.cpp
+++ ECE160/Projects/Project02/convert.cpp
@@ -2,12 +2,23 @@
 I, Daniel Noyes, certify that this is my own work and I have not collaborated with anyone else.
 I encourage the use of this program as OPEN SOURCE.
 */
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
 
 float initial, final, inc, k, r, f, s;
+/*
+variables meaning:
+initial=initial value
+final=final value
+inc=increment value
+k=kilometers/hour
+r=rods/min
+f=furlongs/fortnight
+s=smooths/second
+*/
 printf("Welcome to the convert program\n");
 printf("This will produce a chart showing equivalent for the following:\n");
 printf("=============\nKilometers/hours\nrods/min\nfurlongs/fortnight\nsmoots/seconds\n\n=============\n");
@@ -22,15 +33,18 @@ scanf("%f",&inc);
 
     while (inc != 0.0)
     {
+		printf("_________________________________________________________________\n");
         printf("           Km/Hr        Rods/Min  Frlngs/Frtnght      Smoots/sec\n");
+		printf("_________________________________________________________________\n");
         for (k=initial; k<=final; k=k+inc)
         {
             r=k*3.314;
             f=k*1670.24576;
             s=k*0.163225865;
-            printf("%16.2f%16.2f%16.2f%16.2f\n",k,r,f,s);      
+            printf("|%15.2f|%15.2f|%15.2f|%15.2f|\n",k,r,f,s);    
+			//the %16.2f means the float is allocated 16 spaces in the print function with two decimal spaces.
         }
-    
+		printf("|_______________|_______________|_______________|_______________|\n");
     
     
         printf("Enter initial:");
@@ -41,4 +55,5 @@ scanf("%f",&inc);
         scanf("%f",&inc);
 
 }
+		printf("Goodbye");
 }
