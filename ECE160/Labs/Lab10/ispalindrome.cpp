aaac7c434822961793137823f2effefb479e612e
Reiuiji <reiuiji@gmail.com>
2011-12-11 00:47:21 -0500

    Add last lab 10 : palindrome checker

---
+++ ECE160/Labs/Lab10/ispalindrome.cpp
@@ -0,0 +1,55 @@
+/* project 
+I, Daniel Noyes, certify that this is my own work and I have not collaborated with anyone else.
+I encourage the use of this program as OPEN SOURCE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int ispalindrome(char s[]);
+
+
+
+int ispalindrome(char s[])
+{
+	char d[100],Ctmp;
+	//d: character destination, Ctmp: char TEMP
+	int source=0, dest=0,L=0,i;
+	//source=source char, dest=destination char, L:string length
+
+	//This will remove everything but alpha and numerical values
+	while(s[source])
+	{
+		Ctmp=toupper(s[source]);
+
+		if(isalnum(Ctmp))
+		{
+			d[dest]=Ctmp;
+			dest++;
+		}
+
+		source++;
+
+	}
+
+	d[dest]='\0';
+
+	//This will run a palindrome check on the d array
+
+	source=0;
+	dest=0;
+	L=strlen(d);
+
+	for(i=0;i<L/2;i++)
+	{
+		if(d[i]==d[L-1-i])
+			source++;
+		dest++;
+	}
+
+	if(source==dest)
+		return 1;
+	else
+		return 0;
+}
