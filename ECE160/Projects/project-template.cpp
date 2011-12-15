d7332e62b5835af5fb7b2bf722f0a94d68365a0c
Reiuiji <reiuiji@gmail.com>
2011-12-15 18:50:29 -0500

    Forgot to add my project template file

---
+++ ECE160/Projects/project-template.cpp
@@ -0,0 +1,10 @@
+/* project 
+I, Daniel Noyes, certify that this is my own work and I have not collaborated with anyone else.
+I encourage the use of this program as OPEN SOURCE.
+*/
+#include <stdio.h>
+
+int main()
+{
+
+}
